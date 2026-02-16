8cdf5f9d5554e86acafcd96fac091849f349cb4b
bonatolasa <tolboni614@gmail.com>
2026-02-16 11:46:56 +0300

    update role os user schemas

--- src/users/schemas/users.schemas.ts
+++ src/users/schemas/users.schemas.ts
@@ -7,7 +7,7 @@ export class User extends Document {
   @Prop({ required: true })  name: string;
   @Prop({ required: true, unique: true, lowercase: true })  email: string;
   @Prop({ required: true }) password: string;
-  @Prop({ required: true, enum: Role, default: Role.TEAM_MEMBER }) role: Role;
+  @Prop({ required: true, enum: Role, default: Role.MEMBER }) role: Role;
   @Prop({ type: MongooseSchema.Types.ObjectId, ref: 'Team' }) team: string;
   @Prop({ default: true }) isActive: boolean;
   @Prop() lastLogin: Date;
