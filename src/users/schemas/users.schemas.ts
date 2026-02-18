21d493300e61159dcb1d0894022f0dfeedcd3af5
bonatolasa <tolboni614@gmail.com>
2026-02-18 21:10:17 +0300

    user schemas commit

--- src/users/schemas/users.schemas.ts
+++ src/users/schemas/users.schemas.ts
@@ -8,7 +8,7 @@ export class User extends Document {
   @Prop({ required: true, unique: true, lowercase: true })  email: string;
   @Prop({ required: true }) password: string;
   @Prop({ required: true, enum: Role, default: Role.MEMBER }) role: Role;
-  @Prop({ type: MongooseSchema.Types.ObjectId, ref: 'Team' }) team: string;
+  @Prop({ type: MongooseSchema.Types.ObjectId, ref: 'Team' }) team: Types.ObjectId;
   @Prop({ default: true }) isActive: boolean;
   @Prop() lastLogin: Date;
   @Prop() refreshToken: string;
