dc1d89594eddff194afc7fb89ddac50ccca81a42
bonatolasa <tolboni614@gmail.com>
2026-03-11 14:08:23 +0300

    Revert "Merge pull request #1 from bonatolasa/pm_branch"
    This reverts commit 39a658853649eb84f7ace07fd7fcd30a309f6743, reversing
    changes made to 1e91d59e2b4c4baf103d904ba3d37b7f10e8c94c.

--- lib/theme/app_theme.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:project_management/core/constants/app_constants.dart';
-
-class AppTheme {
-  static ThemeData darkblueTheme = ThemeData(
-    scaffoldBackgroundColor: Color(AppConstant.darkThemeBackgroundColor),
-  );
-}
